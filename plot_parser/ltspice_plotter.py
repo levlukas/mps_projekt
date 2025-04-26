6fe144bf7981eda7c2b98dd08f4cfe347e044bf5
levlukas <256660@vutbr.cz>
2025-04-26 12:09:16 +0200

    feat: added multiple stepping handling

--- plot_parser/ltspice_plotter.py
+++ plot_parser/ltspice_plotter.py
@@ -2,32 +2,53 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import os
 
-INP_PATH = f'temp/0_experiment.csv'
-LTSPICE_FILE_NAME = INP_PATH.split('/')[-1].split('.')[0]
+INP_PATHS = ['temp/0_experiment_0.csv', 'temp/0_experiment_1.csv']
+SERIES_NAMES = ['Experiment 0', 'Experiment 1']  # Names for each series
+LTSPICE_FILE_NAME = "combined_plot"
+X_AXIS_TITLE = "X-Axis"
+Y_AXIS_TITLE = "Y-Axis"
+LOG_X_AXIS = False  # Set to True to use a logarithmic scale for the x-axis
+LOG_Y_AXIS = False  # Set to True to use a logarithmic scale for the y-axis
 
-def plot_csv(inp_path):
-    data = pd.read_csv(inp_path)
+def plot_csv(inp_path, series_name):
+    # Read the CSV file, skipping the second line (additional data)
+    data = pd.read_csv(inp_path, skiprows=[1])
+    print(f'Processing file: {inp_path}')
     print(f'Pandas has read the following columns: {data.columns}')
 
-    # plot
-    plt.figure()
     x_axis = data.columns[0]
     for col in data.columns[1:]:
-        plt.plot(data[x_axis], data[col], label=col)
+        plt.plot(data[x_axis], data[col], label=f'{series_name}: {col}')
 
+def main():
+    # Check if all input files exist
+    if len(INP_PATHS) != len(SERIES_NAMES):
+        raise ValueError("The number of input paths must match the number of series names.")
+
+    for inp_path in INP_PATHS:
+        if not os.path.exists(inp_path):
+            raise FileNotFoundError(f'File {inp_path} does not exist')
+
+    # Create the plot
+    plt.figure()
+    for inp_path, series_name in zip(INP_PATHS, SERIES_NAMES):
+        plot_csv(inp_path, series_name)
+
+    # Customize the plot
+    plt.xlabel(X_AXIS_TITLE)
+    plt.ylabel(Y_AXIS_TITLE)
+    if LOG_X_AXIS:
+        plt.xscale('log')
+    if LOG_Y_AXIS:
+        plt.yscale('log')
     plt.legend()
-    plt.title(f'LTspice simulation: {LTSPICE_FILE_NAME}')
+    plt.tight_layout()
 
-    # save plot
+    # Save the plot
     if not os.path.exists('temp'):
         os.mkdir('temp')
     plt.savefig(f'temp/{LTSPICE_FILE_NAME}.png')
-
-def main():
-    if not os.path.exists(INP_PATH):
-        NameError(f'File {INP_PATH} does not exist')    
-    plot_csv(INP_PATH)
-    print(f'Plot saved as {LTSPICE_FILE_NAME}.png')
+    print(f'Plot saved as temp/{LTSPICE_FILE_NAME}.png')
 
 if __name__ == '__main__':
     main()
--- plot_parser/ltspice_to_csv.py
+++ plot_parser/ltspice_to_csv.py
@@ -4,7 +4,7 @@ Reads ltspice planetext file and outputs it as csv to temp folder in plot_parser
 import os
 import pandas as pd
 
-INP_PATH = f'../0_experiment/ltspice/0_experiment.plt'
+INP_PATH = f'../0_experiment/ltspice/0_experiment.txt'
 LTSPICE_FILE_NAME = INP_PATH.split('/')[-1].split('.')[0]
 
 def ltspice_to_csv(inp_path):
@@ -15,7 +15,30 @@ def ltspice_to_csv(inp_path):
     lines = [line.replace('\t', ',') for line in lines]
 
     return lines
-        
+
+def secondary_parser(inp_path:str):
+    """
+    in order to parse the output of ltspice_to_csv()
+    - helpful when more than one param was .stepped
+    """ 
+    no_step_lines = []       
+    header = ''
+
+    with open(inp_path, 'r') as f:
+        lines = f.readlines()
+        header = lines[0]
+        for i, line in enumerate(lines):
+            if "Step Information:" in line:
+                no_step_lines.append(i)
+    
+    no_step_lines.append(len(lines))  # add last line
+    if no_step_lines:
+        for i in range(len(no_step_lines)-1):
+            with open(inp_path[:-4]+f'_{i}.csv', 'w') as f:
+                f.write(header)
+                f.writelines(lines[no_step_lines[i]:no_step_lines[i+1]])
+
+
 
 def main():
     if not os.path.exists(INP_PATH):
@@ -28,7 +51,9 @@ def main():
     with open(f'temp/{LTSPICE_FILE_NAME}.csv','w') as f:
         f.writelines(ltspice_to_csv(INP_PATH))
 
-    
+    # parse secondary
+    secondary_parser(f'temp/{LTSPICE_FILE_NAME}.csv')
+    print(f'File saved as {LTSPICE_FILE_NAME}.csv')   
 
 if __name__ == '__main__':
     main()
